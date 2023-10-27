4611080857fd741751bf871da9f4b03c1b5fe0d2
Kate Cooper | kmcooper <kmcooper@unomaha.edu>
2023-10-27 15:28:24 -0500

    Create improve_this_script.py

---
+++ scripts/improve_this_script.py
@@ -0,0 +1,10 @@
+l = ["apples","bananas","milk","bread"]
+for x in l:
+  print(x)
+
+
+### For the above script, consider the following:
+# Did I name my variables appropriately?
+# Did I tell the user of my code what my code is doing?
+# Did I add comments to my code?
+# Did I format my code correctly/in a readable way?
